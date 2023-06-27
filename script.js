53afd162ef7d7df9d1e71a70ff82142b54d6a2a2
GurgenYeghiazaryan <gugyeghiazaryan@gmail.com>
2023-06-27 19:33:10 +0400

    Wingrade Casino

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector ('.main-categories_category') 
+
+btn.addEventListener('click', () => {
+    btn.style.border = '3px solid rgb(255, 107, 0)'; 
+    btn.style.outline = '1px solid rgb(255, 255, 255)';
+    btn.style.boxShadow = '0px 5px 40px 0px rgba(252, 248, 184, 0.42';
+});
