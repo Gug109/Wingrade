466ba0f9221dc54866583b23c8267f94edd1e482
GurgenYeghiazaryan <gugyeghiazaryan@gmail.com>
2023-07-03 14:10:37 +0400

    burger manu

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-let btn = document.querySelector ('.main-categories_category') 
+let categoryButtons = document.querySelectorAll('.main-categories_category')
+let providerButtons = document.querySelectorAll('.main-providers_provider')
 
-btn.addEventListener('click', () => {
-    btn.style.border = '3px solid rgb(255, 107, 0)'; 
-    btn.style.outline = '1px solid rgb(255, 255, 255)';
-    btn.style.boxShadow = '0px 5px 40px 0px rgba(252, 248, 184, 0.42';
+
+
+function clearCategoryButtonStyles() {
+    categoryButtons.forEach(button => {
+        button.style.border = 'none';
+        button.style.outline = 'none';
+        button.style.boxShadow = 'none';
+    });
+}
+
+categoryButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        clearCategoryButtonStyles();
+        button.style.border = '3px solid rgb(255, 107, 0)';
+        button.style.outline = '1px solid rgb(255, 255, 255)';
+        button.style.boxShadow = '0px 5px 40px 0px rgba(252, 248, 184, 0.42)';
+    });
 });
+
+////////////
+
+function clearProviderButtonStyles() {
+    providerButtons.forEach(button => {
+        button.style.border = 'none';
+        button.style.outline = 'none';
+        button.style.boxShadow = 'none';
+    });
+}
+
+providerButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        clearProviderButtonStyles();
+        button.style.border = '3px solid rgb(255, 107, 0)';
+        button.style.outline = '1px solid rgb(255, 255, 255)';
+        button.style.boxShadow = '0px 5px 40px 0px rgba(252, 248, 184, 0.42)';
+    });
+});
+
+
