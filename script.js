574371bc1fef8202e4de2086e6c2e36b9a730c99
GurgenYeghiazaryan <gugyeghiazaryan@gmail.com>
2023-07-04 17:08:09 +0400

    .

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ let burgerBtn = document.querySelector(".main-header_opened-btn");
 let burgerBlock = document.querySelector(".main-nav_burger-ul");
 let closeBtn = document.querySelector(".main-nav_close-btn");
 let fireImg = document.querySelector(".main-header_fire-img");
- 
+let dropdownBtn = document.querySelector(".main-header_dropdown-arrow-btn");
+let languageBlock = document.querySelector(".main-header_language-block");
+
+dropdownBtn.onclick = function() {
+    dropdownBtn.classList.toggle("active");
+    languageBlock.style.display = "block";
+    dropdownBtn.style.transform = "rotate(180deg)";
+    dropdownBtn.style.marginLeft = "6px";
+}
 
 closeBtn.addEventListener('click', () => {
     burgerBlock.style.display = "none";
@@ -18,25 +26,25 @@ burgerBtn.addEventListener('click', () => {
     burgerBlock.style.zIndex = "10003";
     fireImg.style.display = "none";
 
-} ) 
+}) 
 
 
 function clearCategoryButtonStyles() {
-    categoryButtons.forEach(button => {
-        button.style.border = 'none';
-        button.style.outline = 'none';
-        button.style.boxShadow = 'none';
-    });
-}
+        categoryButtons.forEach(button => {
+            button.style.border = 'none';
+            button.style.outline = 'none';
+            button.style.boxShadow = 'none';
+        });
+    }
 
 categoryButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        clearCategoryButtonStyles();
-        button.style.border = '3px solid rgb(255, 107, 0)';
-        button.style.outline = '1px solid rgb(255, 255, 255)';
-        button.style.boxShadow = '0px 5px 40px 0px rgba(252, 248, 184, 0.42)';
+        button.addEventListener('click', () => {
+            clearCategoryButtonStyles();
+            button.style.border = '3px solid rgb(255, 107, 0)';
+            button.style.outline = '1px solid rgb(255, 255, 255)';
+            button.style.boxShadow = '0px 5px 40px 0px rgba(252, 248, 184, 0.42)';
+        });
     });
-});
 
 ////////////
 
